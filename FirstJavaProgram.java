2fb03e8f8790372facf0a9be04db659c9a8b04db
Muhammad Munir <MuhammadShaMunir@gmail.com>
2019-06-12 13:16:34 -0500

    Add files via upload

---
+++ FirstJavaProgram.java
@@ -0,0 +1,7 @@
+public class FirstJavaProgram {
+    public static void main(String[] args) {
+       System.out.println("My name is Muhammad Munir"); 
+       System.out.println("I am 22 years old");
+       System.out.println("My hometown is Islamabad, Pakistan");
+    }   
+}
